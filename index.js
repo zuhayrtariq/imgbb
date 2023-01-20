72dbcc3c5ea35178a5830373aa6dea0aecc0b77b
zuhayrtariq <zuhayrtariq4@gmail.com>
2023-01-20 08:54:35 +0500

    Added res

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ app.use(cors());
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
-
+app.get('/data', function (req, res) {
+  res.send('I am working!!!!');
+});
 app.listen(3000);
